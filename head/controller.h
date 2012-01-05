88a8fcc5cbba202b46c9e4aa82ad2b3ac33d1b81
Andriy_Lin <lxk19921205@gmail.com>
2012-01-05 15:44:38 +0800

    增加了header目录，增加了CController类

---
+++ head/controller.h
@@ -0,0 +1,22 @@
+/*
+ * controller.h
+ *
+ *  Created on: 2012-1-5
+ *      Author: andriy
+ */
+
+#ifndef CONTROLLER_H_
+#define CONTROLLER_H_
+
+
+/**
+ * 在后台默默控制的类
+ */
+class CController
+{
+public:
+	CController();
+};
+
+
+#endif /* CONTROLLER_H_ */
---
+++ src/controller.cpp
@@ -0,0 +1,14 @@
+/*
+ * controller.cpp
+ *
+ *  Created on: 2012-1-5
+ *      Author: andriy
+ */
+
+#include "../head/controller.h"
+
+
+CController::CController()
+{
+
+}
